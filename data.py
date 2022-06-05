131c0a1ac4a3b36bcc67e8d31a4a4f18644768e9
Yongho Kim <52735725+kimy-de@users.noreply.github.com>
2022-06-05 20:23:02 +0200

    ac

--- data.py
+++ data.py
@@ -38,3 +38,44 @@ def bg_generator(dt, dx, N_u=0, typ='train'):
    
     else:
         return t_data_f, x_data_f
+
+def ac_generator(dt, dx, N_u=0, typ='train'):
+    N_f = int((1/dt)*(1/dx))
+    num_init_cond = int(N_u*0.7)
+    num_time = N_u - num_init_cond
+    
+    t = np.linspace(0, 1, int(1/dt)).reshape(-1,1) # T x 1
+    x = np.linspace(-1, 1, int(1/dx)).reshape(-1,1) # N x 1
+    T = t.shape[0]
+    N = x.shape[0]
+    T_star = np.tile(t, (1, N)).T  # N x T
+    X_star = np.tile(x, (1, T))  # N x T
+    
+    # Initial condition and boundary condition
+    u = np.zeros((N, T))  # N x T
+    u[:,0:1] = (x**2)*np.cos(np.pi*x)
+    u[0,:] = -np.ones(T) 
+    u[-1,:] = u[0,:]
+    
+    t_data = T_star.flatten()[:, None]
+    x_data = X_star.flatten()[:, None]
+    u_data = u.flatten()[:, None]
+    
+    t_data_f = t_data.copy()
+    x_data_f = x_data.copy()
+    
+    if typ == 'train':
+        idx = np.random.choice(np.where((x_data == -1) | (x_data == 1))[0], num_time)
+        t_data = t_data[idx]
+        x_data = x_data[idx]
+        u_data = u_data[idx]
+        
+        init_idx = np.random.choice(N-1, num_init_cond-4, replace=False) + 1
+        t_data = np.concatenate([t_data, np.ones((2,1)), np.zeros((num_init_cond-4,1))], axis=0)
+        x_data = np.concatenate([x_data, np.array([[-1], [1]]), x[init_idx]], axis=0)
+        u_data = np.concatenate([u_data, -np.ones((2,1)), u[init_idx,0:1]], axis=0)
+        
+        return t_data, x_data, u_data, t_data_f, x_data_f
+   
+    else:
+        return t_data_f, x_data_f
