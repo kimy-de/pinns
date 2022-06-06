9a939052a9cd56c5e1402912f3d20ba7893fe1eb
Yongho Kim <52735725+kimy-de@users.noreply.github.com>
2022-06-06 09:27:22 +0200

    resplot

--- utils.py
+++ utils.py
@@ -21,3 +21,46 @@ def ac_equation(u, tx):
     u_xx = fwd_gradients(u_x, tx)[:, 1:2]
     e = u_t -0.0001*u_xx + 5*u**3 - 5*u
     return e 
+
+def resplot(x, t, t_data, x_data, Exact, u_pred, eq):
+    plt.figure(figsize=(10, 10))
+    plt.subplot(2, 2, 1)
+    plt.plot(x, Exact[:,0],'-')
+    plt.plot(x, u_pred[:,0],'--')
+    plt.legend(['Actual', 'Prediction'])
+    plt.title("Initial condition ($t=0$)")
+    
+    plt.subplot(2, 2, 2)
+    t_step = int(0.25*len(t))
+    plt.plot(x, Exact[:,t_step],'-')
+    plt.plot(x, u_pred[:,t_step],'--')
+    plt.legend(['Actual', 'Prediction'])
+    plt.title("$t=0.25$")
+    
+    plt.subplot(2, 2, 3)
+    t_step = int(0.5*len(t))
+    plt.plot(x, Exact[:,t_step],'-')
+    plt.plot(x, u_pred[:,t_step],'--')
+    plt.legend(['Actual', 'Prediction'])
+    plt.title("$t=0.5$")
+    
+    plt.subplot(2, 2, 4)
+    t_step = int(0.99*len(t))
+    plt.plot(x, Exact[:,t_step],'-')
+    plt.plot(x, u_pred[:,t_step],'--')
+    plt.legend(['Actual', 'Prediction'])
+    plt.title("$t=0.99$")
+    plt.savefig('./inference_'+eq+'.png')
+    
+    plt.figure(figsize=(10, 5))
+    plt.imshow(u_pred, interpolation='nearest', cmap='jet',
+               extent=[t.min(), t.max(), x.min(), x.max()],
+               origin='lower', aspect='auto')
+    plt.clim(-1, 1)
+    plt.ylim(-1,1)
+    plt.xlim(0,1)
+    plt.scatter(t_data, x_data)
+    plt.xlabel('t')
+    plt.ylabel('x')
+    plt.title('u(t,x)')
+    plt.savefig('./evolution_'+eq+'.png')
